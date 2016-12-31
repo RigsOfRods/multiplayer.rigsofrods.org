7d55afde1322ec59b2229bdc926767446c236321
Petr Ohlídal <petr@ohlidal.cz>
2016-12-31 04:22:57 +0100

    :sparkles: Added main page, i.e. "portal"
    Displays serverlist and total number of servers / players.
    Shows "under development" banner.

---
+++ publish/globals.include.php
@@ -0,0 +1,7 @@
+<?php
+
+define('LOG_LEVEL_ERROR',   0);
+define('LOG_LEVEL_WARNING', 1);
+define('LOG_LEVEL_INFO',    2);
+define('LOG_LEVEL_DETAIL',  3);
+define('LOG_LEVEL_DEBUG',   4);
--- publish/server-list.php
+++ publish/server-list.php
@@ -1,11 +1,6 @@
 <?php
 
-define('LOG_LEVEL_ERROR',   0);
-define('LOG_LEVEL_WARNING', 1);
-define('LOG_LEVEL_INFO',    2);
-define('LOG_LEVEL_DETAIL',  3);
-define('LOG_LEVEL_DEBUG',   4);
-
+require 'globals.include.php';
 require 'config.include.php';
 
 function log_msg($level, $msg)
